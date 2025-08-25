57de723ad5c63471e7138809b64177c2a9aec7fd
tncks <duddndtncks@naver.com>
2025-08-25 21:15:00 +0900

    feat: google oauth

--- packages/server/server.js
+++ packages/server/server.js
@@ -4,7 +4,7 @@ const { initializeStockData } = require('./src/services/stock.service');
 const port = process.env.PORT || 3000;
 
 // Initialize data on startup
-initializeStockData();
+/* * * * initializeStockData(); * * * disabled temporarily * */
 
 app.listen(port, () => {
   console.log(`Server running at http://0.0.0.0:${port}`);
--- packages/server/src/api/v1/routes/authRoutes.js
+++ packages/server/src/api/v1/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createClient } = require('@supabase/supabase-js');
+const {createClient} = require('@supabase/supabase-js');
 const supabaseUrl = process.env.MY_SUPABASE_URL;
 const supabaseAnonKey = process.env.MY_SUPABASE_ANON_KEY;
 
@@ -9,27 +9,27 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 router.get('/google', async (req, res) => {
     // 프로덕션/개발 환경에 따른 서버 URL 설정
-        const serverUrl = process.env.SERVER_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : `${req.protocol}://${req.get('host')}`);
+    const serverUrl = `http://49.50.132.4:3000`;
 
-   const redirectTo = `${serverUrl}/api/v1/callback/google`;
+    const redirectTo = `${serverUrl}/api/v1/callback/google`;
 
-   const { data,error } = await supabase.auth.signInWithOAuth({
-       provider: 'google',
-       options: {
-           redirectTo,
-           // queryParams: {
-           //     access_type: 'offline',
-           //     prompt: 'consent',
-           // },
-       },
-   });
+    const {data, error} = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+            redirectTo,
+            // queryParams: {
+            //     access_type: 'offline',
+            //     prompt: 'consent',
+            // },
+        },
+    });
 
-   if(error) {
-       console.error('Error signin in with Google:', error);
-       return res.status(500).json({ error : 'Internal server error 500'})
-   }
+    if (error) {
+        console.error('Error signin in with Google:', error);
+        return res.status(500).json({error: 'Internal server error 500'})
+    }
 
-   res.redirect(data.url);
+    res.redirect(data.url);
 });
 
 module.exports = router;
